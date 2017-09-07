4066a52e0164010e424bd2a2a95d4280d230fc7e
Rikard Eide <rix1@otovo.no>
2017-09-07 21:55:52 +0200

    Added support for styled-components, classNames, and svg loading
    Added navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,39 @@
+import styled from 'styled-components';
+import classNames from 'classnames';
 import Link from 'next/link';
 import Logo from '../svgs/logo.svg';
 
+const ItemList = ({ children, className }) => (
+  <div className={classNames('dtc v-mid w-75 tr', className)}>{children}</div>
+);
+
+const StyledList = styled(ItemList)`
+  > a:last-child {
+    margin-right: 0;
+  }
+`;
+
+const NavItem = ({ to = '/', children, ...rest }) => (
+  <Link href={to}>
+    <a className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" title={children} {...rest}>
+      {children}
+    </a>
+  </Link>
+);
+
 const Navbar = props => {
   return (
-    <nav className="db dt-l w-100 border-box pa3 ph5-l">
-      <Link href="/">
+    <nav className="dt w-100 border-box pa3 ph5-ns bg-white card-shadow">
+      <Link prefetch href="/">
         <a className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" title="Home">
-          <Logo className="w4 h-auto"/>
+          <Logo className="w4 h-auto" />
         </a>
       </Link>
-      <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-        <Link href="/">
-          <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" title="Home">Home</a>
-        </Link>
-        <Link href="/about">
-          <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" title="Home">Om oss</a>
-        </Link>
-        <Link href="/photo-essay">
-          <a className="link dim dark-gray f6 f5-l dib" title="Home">Foto</a>
-        </Link>
-      </div>
+      <StyledList className="dtc v-mid w-75 tr">
+        <NavItem to="#product">Product</NavItem>
+        <NavItem to="#team">Team</NavItem>
+        <NavItem to="#newsletter">Newsletter</NavItem>
+      </StyledList>
     </nav>
   );
 };

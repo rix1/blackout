7e793b3f85f01c512462fd57959bcfeef2c4cb87
Siri Holtnæs <siri.holtnaes@schibsted.com>
2017-09-17 13:01:54 +0200

    Started on map page

---
+++ pages/googlemaps/index.js
@@ -0,0 +1,69 @@
+import { compose, withProps, withState, withHandlers } from "recompose";
+
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from "react-google-maps";
+
+const MapWithControlledZoom = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withState('zoom', 'onZoomChange', 8),
+  withHandlers(() => {
+    const refs = {
+      map: undefined,
+    }
+
+    return {
+      onMapMounted: () => ref => {
+        refs.map = ref
+      },
+      onZoomChanged: ({ onZoomChange }) => () => {
+        onZoomChange(refs.map.getZoom())
+      }
+    }
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultCenter={{ lat: 59.9, lng: 10.7 }}
+    zoom={props.zoom}
+    ref={props.onMapMounted}
+    onZoomChanged={props.onZoomChanged}
+  >
+    <Marker
+      position={{ lat: 59.9, lng: 10.7 }}
+      onClick={props.onToggleOpen}
+    >
+      <InfoWindow onCloseClick={props.onToggleOpen}>
+        <div>
+          {" "}
+          Controlled zoom: {props.zoom}
+        </div>
+      </InfoWindow>
+    </Marker>
+    <Marker
+      position={{ lat: 60, lng: 10.7 }}
+      onClick={props.onToggleOpen}
+    >
+      <InfoWindow onCloseClick={props.onToggleOpen}>
+        <div>
+          {" "}
+          Controlled zoom: {props.zoom}
+        </div>
+      </InfoWindow>
+    </Marker>
+
+  </GoogleMap>
+);
+
+<MapWithControlledZoom />
+export default MapWithControlledZoom;
